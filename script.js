0803a9653631f4a8bff5382e60c825ef01691621
amarmann92 <73195170+amarmann92@users.noreply.github.com>
2021-01-14 14:36:27 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+  var characters = ["q","t","e","a","l","z"];
+ var passwordlength = parseInt(prompt("how many characters for your password?"))
+ var numbersreq = confirm("Do you need number in your password?")
+ console.log(numbersreq)
+  console.log(typeof passwordlength)
+
+var final = []
+
+  for (let i = 0; i < passwordlength; i++) {
+    var x = Math.floor(Math.random()*characters.length)
+    final.push(characters[x])
+  }
+  console.log(final)
+  return(final.join(""))
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
